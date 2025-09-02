78d1ece8c83ea7cf15e5353ff6584e5f07d52bf8
Eyob <eyutialemu@gmail.com>
2025-09-02 09:14:20 +0300

    Create joins_queries.sql

---
+++ database-adv-script/joins_queries.sql
@@ -0,0 +1,35 @@
+--INNER JOIN--
+SELECT 
+    b.booking_id,
+    u.user_id,
+    u.name AS user_name,
+    b.property_id,
+    b.check_in,
+    b.check_out,
+    b.status
+FROM Bookings b
+INNER JOIN Users u 
+  ON b.user_id = u.user_id;
+
+-- Left Join ---
+SELECT 
+    p.property_id,
+    p.title AS property_title,
+    r.review_id,
+    r.rating,
+    r.comment
+FROM Properties p
+LEFT JOIN Reviews r 
+    ON p.property_id = r.property_id;
+--Full outer join--
+SELECT 
+    u.user_id,
+    u.name AS user_name,
+    b.booking_id,
+    b.property_id,
+    b.check_in,
+    b.check_out,
+    b.status
+FROM Users u
+FULL OUTER JOIN Bookings b
+    ON u.user_id = b.user_id;
