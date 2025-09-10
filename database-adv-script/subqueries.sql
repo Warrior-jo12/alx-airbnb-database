f361bfabd415a629501835e571f3f479dab5b774
Eyob <eyutialemu@gmail.com>
2025-09-10 08:26:04 +0300

    Create subqueries.sql

---
+++ database-adv-script/subqueries.sql
@@ -0,0 +1,27 @@
+
+SELECT 
+    p.property_id,
+    p.title,
+    p.location
+FROM Properties p
+WHERE p.property_id IN (
+    SELECT r.property_id
+    FROM Reviews r
+    GROUP BY r.property_id
+    HAVING AVG(r.rating) > 4.0
+)
+ORDER BY p.title ASC;
+
+
+SELECT 
+    u.user_id,
+    u.name,
+    u.email
+FROM Users u
+WHERE (
+    SELECT COUNT(*)
+    FROM Bookings b
+    WHERE b.user_id = u.user_id
+) > 3
+ORDER BY u.name ASC;
+
